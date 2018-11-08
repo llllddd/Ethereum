7b924423d6184dfbf9f12882c9803657ad787666
lidian <1048202897@qq.com>
2018-11-08 16:15:55 +0800

    Dilithium

--- xchain-go/utils/constvalue.go
+++
@@ -1,8 +0,0 @@
-package utils
-
-import "math/big"
-
-var (
-	ChainID  = big.NewInt(1)     //固定写为主网的1
-	GasPrice = big.NewInt(21000) //固定值-gasprice
-)
--- xchain-go/utils/utils.go
+++
@@ -1,9 +0,0 @@
-package utils
-
-import "encoding/binary"
-
-func EncodeBlockNumber(number uint64) []byte {
-	enc := make([]byte, 8)
-	binary.BigEndian.PutUint64(enc, number)
-	return enc
-}
